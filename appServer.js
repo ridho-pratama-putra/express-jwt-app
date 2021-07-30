72b312bf5fca4c01802c692cb8471a6a5b89c132
ridhopratamavd@gmail.com <ridhopratamavd@gmail.com>
2021-07-30 07:46:06 +0700

    fix lint issues

--- appServer.js
+++ appServer.js
@@ -1,39 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const appServer = express();
-const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const express = require('express')
+const appServer = express()
+const jwt = require('jsonwebtoken')
 
-appServer.use(express.json());
+appServer.use(express.json())
 
 appServer.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 function authenticateUser (req, res, next) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
   if (token == null) {
-    return res.sendStatus(401);
+    return res.sendStatus(401)
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
-      console.log(err);
-      return res.sendStatus(403);
+      console.log(err)
+      return res.sendStatus(403)
     }
-    req.user = user;
-    next();
-  });
+    req.user = user
+    next()
+  })
 }
 
 appServer.get('/posts', authenticateUser, (req, res) => {
   res.json([{
     content: 'first  post',
-    creator: 'user A'
+    creator: 'user A',
   }, {
     content: 'second  post',
-    creator: 'user B'
+    creator: 'user B',
   }].filter(post => post.creator === req.user.name)
-  );
-});
-module.exports = appServer;
+  )
+})
+module.exports = appServer
--- authServer.js
+++ authServer.js
@@ -1,77 +1,77 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const jwt = require('jsonwebtoken');
-const User = require('./models/user');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const User = require('./models/user')
 
-app.use(express.json());
-let refreshTokens = [];
+app.use(express.json())
+let refreshTokens = []
 
 app.post('/token', (req, res) => {
-  const refreshToken = req.body.token;
+  const refreshToken = req.body.token
   if (refreshToken == null) {
-    return res.sendStatus(401);
+    return res.sendStatus(401)
   }
   if (!refreshTokens.includes(refreshToken)) {
-    return res.sendStatus(403);
+    return res.sendStatus(403)
   }
   jwt.verify(refreshToken, process.env.REFRESH_ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.sendStatus(403)
     }
-    const accessToken = generateAccessTokenWithExipration({ name: user.name });
-    res.json({ accessToken });
-  });
-});
+    const accessToken = generateAccessTokenWithExipration({ name: user.name, })
+    res.json({ accessToken, })
+  })
+})
 
 app.delete('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
-  res.sendStatus(204);
-});
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
 
 function generateAccessTokenWithExipration (user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s' });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s', })
 }
 
 app.post('/login', (req, res) => {
   // authenticate user
-  const { username } = req.body;
-  const user = { name: username };
-  const accessToken = generateAccessTokenWithExipration(user);
+  const { username, } = req.body
+  const user = { name: username, }
+  const accessToken = generateAccessTokenWithExipration(user)
 
-  const refreshToken = jwt.sign(user, process.env.REFRESH_ACCESS_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_ACCESS_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
   res.json({
     accessToken,
-    refreshToken
-  });
-});
+    refreshToken,
+  })
+})
 
 app.post('/register', async (req, res) => {
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
-  });
+    password: req.body.password,
+  })
 
   await user.save((err, doc) => {
     if (err) {
-      console.log(err);
+      console.log(err)
       res.status(400).json({
         status: '06',
-        description: 'failed to crate account'
-      });
-      return;
+        description: 'failed to crate account',
+      })
+      return
     }
 
     res.status(201).json({
       status: '00',
       description: 'success crate account',
       response: {
-        doc
-      }
-    });
-  });
-});
+        doc,
+      },
+    })
+  })
+})
 
-module.exports = app;
+module.exports = app
