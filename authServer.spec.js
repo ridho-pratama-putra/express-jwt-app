c726b2d67ad6f8061fdb36cf6a0659f59e3a68c3
ridhopratamavd@gmail.com <ridhopratamavd@gmail.com>
2021-07-30 12:58:32 +0700

    test for registration when submit without email

--- authServer.spec.js
+++ authServer.spec.js
@@ -2,8 +2,13 @@ const request = require('supertest');
 const app = require('./authServer'); // the express server
 
 describe('AuthServer', () => {
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
   describe('/login', () => {
-    it('return array of object contain token and refresh token', async () => {
+    it('should return array of object contain token and refresh token', async () => {
       await request(app)
         .post('/login').send({
           username: 'user A'
@@ -21,14 +26,14 @@ describe('AuthServer', () => {
   });
 
   describe('/token', () => {
-    it('return 401 when no refresh token listed', async () => {
+    it('should return 401 when no refresh token listed', async () => {
       await request(app)
         .post('/token').send({
           token: null
         })
         .expect(401);
     });
-    it('return 403 when no refresh token listed', async () => {
+    it('should return 403 when no refresh token listed', async () => {
       await request(app)
         .post('/token').send({
           token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidXNlciBBIiwiaWF0IjoxNjI3MzEwOTYxfQ.QAETcsieJblDV2jZ2seg4iZEKjcWfAlYQcRHGamDKoc'
@@ -38,12 +43,25 @@ describe('AuthServer', () => {
   });
 
   describe('/logout', () => {
-    it('return 204 after delete refresh token', async () => {
+    it('should return 204 after delete refresh token', async () => {
       await request(app)
         .delete('/logout').send({
           token: 'fake invalid token'
         })
         .expect(204);
+    });
+  });
+
+  describe('/register', () => {
+    it('should return 400 failed to create account when required email not fulfilled', async () => {
+      const res = await request(app)
+        .post('/register').send({
+          username: 'my username',
+          password: 'p@ssw0d'
+        })
+        .expect(400)
+        expect(res.body.status.code).toEqual('06')
+        expect(res.body.status.description).toEqual('failed to crate account')
     });
   });
 });
